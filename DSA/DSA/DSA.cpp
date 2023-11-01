827410817fb05f86b469fbb41b0955eab061b192
Shubham <shubham.a.more20@gmail.com>
2023-11-01 13:27:27 -0600

    reversing an array, arranging an array implemented

--- DSA/DSA/DSA.cpp
+++ DSA/DSA/DSA.cpp
@@ -7,6 +7,13 @@
 
 int main()
 {
+    int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+    int* op = reverseArray(arr, 10);
+
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << op[i] << " ";
+    }
     std::cout << "Hello World!\n";
 }
 
--- DSA/DSA/array.h
+++ DSA/DSA/array.h
@@ -2,9 +2,9 @@
 
 int linearSearch(int arr[], int key, int size);
 int binarySearch(int arr[], int low, int high, int key, int size);
-void reverseArray(int arr[]);
+int* reverseArray(int arr[], int size);
 bool isArraySorted(int arr[], int size);
-void arrangeArray(int arr[]);
+int* arrangeArray(int arr[], int size);
 
 int linearSearch(int arr[], int key, int size)
 {
@@ -39,12 +39,19 @@ int binarySearch(int arr[], int low, int high, int key, int size)
 	}
 }
 
-void reverseArray(int arr[])
+int* reverseArray(int arr[], int size)
 {
+	int* op = new int[size];
+	int i, j;
 
+	for (i = size - 1, j = 0; i >= 0; i--, j++)
+	{
+		op[j] = arr[i];
+	}
+	return op;
 }
 
-bool isArraySorted(int arr[], int size)
+bool isArraySorted(int arr[], int size) //checks if array is sorted in ascending order
 {
 	for (int i = 0; i < size - 1; i++)
 	{
@@ -54,7 +61,25 @@ bool isArraySorted(int arr[], int size)
 	return false;
 }
 
-void arrangeArray(int arr[])
+int* arrangeArray(int arr[], int size) //shifts all the negative elements to the left and positve elements to the right
 {
+	int i = 0;
+	int j = size - 1;
+	int temp;
 
+	while (i < j)
+	{
+		while (arr[j] < 0) { j--; }
+		while (arr[i] >= 0) { i++; }
+
+		if (i < j)
+		{
+			//swap
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+	}
+
+	return arr;
 }
